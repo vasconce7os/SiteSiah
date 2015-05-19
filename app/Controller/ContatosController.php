da95f64d51e66aa6a89305128ac742f8f16f9f39
Cerebro Vasconcelos <cerebro.vasconcelos@zipmail.com.br>
2015-05-19 11:49:09 -0400

    Só tirando comentários
    e otimizando o arquivo

--- app/Controller/ContatosController.php
+++ app/Controller/ContatosController.php
@@ -4,9 +4,93 @@
 
 App::uses('AppController', 'Controller');
 
+<?php
+
+
+
+App::uses('AppController', 'Controller');
+
 
 class ContatosController extends AppController 
 {
+
+	//public $uses = array();
+	var $components = array("Email");
+
+	public function index() 
+	{
+		$this-> set('title_for_layout', "Contato com a SIAH");
+		/*
+		já não precisa mais, estão no layout default
+		$this-> cssExtra[] = array('file'=> 'css/bootstrap.min', 'comment'=> "Css 2 col", 'shortPath'=> true, 'media'=> "all");
+		$this-> cssExtra[] = array('file'=> 'css/font-awesome.min', 'comment'=> "icones no formato de font", 'shortPath'=> true, 'media'=> "all");
+		$this-> set('cssExtra', $this-> cssExtra);
+		*/
+	}
+
+	public function enviar()
+	{
+		if($this-> request-> is("post"))
+		{
+			$contatoForDB = $this-> data;
+			$contatoForDB['Contato']['cliente_id'] = 1; // cliente não identificado
+			$enviou = $this-> Contato-> save($contatoForDB);
+			$data = $this-> request-> data;
+			if($enviou)
+			{
+				$this->autoRender = false;
+				$view = new View($this, false);
+				$view->set('contatoInDB', $data);
+				$view->viewPath = 'Contatos';
+				//$this-> layout = 'ajax';
+				$conteudo = $view->render('enviar', 'ajax');
+				$retEmail =        true; //$this-> enviaEmail("sitesiah@siahonline.com", 'Contato via site de: '.@$data['Contato']['nome'], $conteudo, 'O seu estoque');
+				$retEmailCliente = $this-> enviaEmail($data['Contato']['email'], 'Recebemos seu contato '.@$data['Contato']['nome'], $conteudo, $this-> sisCliente['nome']);
+				/*
+				if($retEmail == true)
+				{
+					$this->log('Enviou para o Adm');
+					$this-> Session->setFlash('Obrigado por nos contactar '.@$data['Contato']['nome']
+							, 'default'
+							, array('class' => 'noticeSuccess')); //noticeSuccess, notice, message
+					$this-> redirect(array("controller"=> "contatos", "action"=> "index"));
+				} else
+				{
+					$this->log('Não enviou para o adm');
+					$this-> Session->setFlash('Houve um erro, você pode tentar novamente ou tentar outras formas de contato!'
+							, 'default'
+							, array('class' => 'message')); //noticeSuccess, notice, message
+				}
+				*/
+				if($retEmail == true)
+				{
+					$this->log('Enviou para o contatante');
+					$this-> Session->setFlash('Obrigado por nos contactar '.@$data['Contato']['nome']
+							, 'default'
+							, array('class' => $this-> successMsgClass)); 
+					$this-> redirect(array("controller"=> "pages", "action"=> "inicio"));
+				} else
+				{
+					$this->log('Não enviou para o contatante');
+					$this-> Session->setFlash('Houve um erro, você pode tentar novamente ou tentar outras formas de contato!'
+							, 'default'
+							, array('class' => $this-> infoMsgClass)); 
+					$this-> redirect(array("controller"=> "atendimentos", "action"=> "index"));
+				}
+			}
+			else
+			{
+				$this-> Session->setFlash('Houve um erro!'
+						, 'default'
+						, array('class' => $this-> errorMsgClass)); 
+			}
+			/* */
+		} else 
+		{
+			$this-> log("no enviar: NÃO é post ");
+		}
+	}
+}
 
 	//public $uses = array();
 	var $components = array("Email");
