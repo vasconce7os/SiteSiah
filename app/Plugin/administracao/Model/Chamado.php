6ecde974efbddc13ef641d1459e1db58962e84cc
Cerebro Vasconcelos <cerebro.vasconcelos@zipmail.com.br>
2015-06-30 09:00:08 -0400

    Use minha query para pegar as msgs dos chamados

--- app/Plugin/administracao/Model/Chamado.php
+++ app/Plugin/administracao/Model/Chamado.php
@@ -15,5 +15,28 @@ class Chamado extends AdministracaoAppModel
             'className' => 'User'
         )
     );
+
+
+    public function getMessagesChamado($idChamado)
+    {
+        $idChamado = (int) $idChamado;
+
+        $sql = "
+            select Chamadomsg.id, Chamadomsg.msg, Chamadomsg.created
+            , Chamadomsg.chamado_id
+            , User.id, User.username 
+               , (select count(id)
+                from admins Admin
+                where user_id = User.id) qtdeAdmin
+               , if((select count(id)
+                from admins Admin
+                where user_id = User.id) > 0,'Suporte', 'Cliente') tipoUsuario
+            from chamadomsgs Chamadomsg 
+            join users User on User.id = Chamadomsg.user_id 
+            where Chamadomsg.chamado_id = $idChamado 
+            order by Chamadomsg.id
+            ";
+        return $this->query($sql);
+    }
     
 }
