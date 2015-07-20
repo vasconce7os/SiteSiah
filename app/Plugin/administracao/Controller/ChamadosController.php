3c6d1ae2f63f0af4f0d901b90dbaf5d25d008133
Cerebro Vasconcelos <cerebro.vasconcelos@zipmail.com.br>
2015-07-20 11:00:07 -0400

    Defina o adm/suporte responsável pelo chamado
    E também corrija algusn bugs na alteração de status no responder() de
    adm/suporte e limpe o código

--- app/Plugin/administracao/Controller/ChamadosController.php
+++ app/Plugin/administracao/Controller/ChamadosController.php
@@ -14,14 +14,11 @@ class ChamadosController extends AdministracaoAppController
 		);
 		$this-> paginate = $options;
 		$lChamados = $this-> paginate('Chamado');
-		//pr($lChamados);
-		//exit;
 		if(!$lChamados)
 		{
 			$this-> Session-> setFlashAtencao("Não há mensagens de chamados para exibir!");
 		}
 		$this-> set('lChamados', $lChamados);
-		//$this-> set('totalChamados', $this-> Chamado-> find('count'));
 		$this-> set('title_for_layout', "Listando mensagens chamados");
 	}
 	
@@ -37,10 +34,6 @@ class ChamadosController extends AdministracaoAppController
 		{
 			$this-> Session-> setFlashErro("Chamado não encontrado!");
 			$this-> redirect(array('action'=> 'index'));
-		} else
-		{
-			//pr($this-> Chamado-> data['Chamado']['status']);
-			//exit;
 		}
 		$chamado['Chamadomsg'] = $this-> Chamado-> getMessagesChamado($id);
 		$chamado['Cliente'] = $this-> Cliente-> findById($chamado['User']['cliente_id'], array('field'=> "fantasia", "id", "cnpj", "naoativo"));
@@ -49,7 +42,6 @@ class ChamadosController extends AdministracaoAppController
 			$chamado['Cliente'][$key] = $propriedadeCliente;
 		}
 		unset($chamado['Cliente']['Cliente']);
-		//pr($chamado['Cliente']); exit;
 		$this-> request-> data = $chamado;
 		$this-> set('chamado', $chamado);
 		$this-> set('title_for_layout', $chamado['Chamado']['titulo']);
@@ -66,29 +58,21 @@ class ChamadosController extends AdministracaoAppController
 		}
 		if($this-> request-> is("post"))
 		{
-			//pr($chamado);
-			//pr($this-> request-> data);
-			//exit;
 			$chamadoForDB['Chamado']['id'] = $this-> Chamado-> id;
-			//$chamadoForDB['Chamado']['admin_id'] = $this-> logged-> Admin-> id; // ja foi setado no iniciodo chamado
-			if($chamado['Chamado']['status'] = 'aguardando análise inicial')
+			if(utf8_encode($chamado['Chamado']['status']) == 'aguardando análise inicial')
 			{
-				$chamadoForDB['Chamado']['status'] = 'aguardando cliente';
+				$chamadoForDB['Chamado']['admin_id'] = $this-> logged-> Admin-> id;
 			}
-
+			$chamadoForDB['Chamado']['status'] = 'aguardando cliente';
 			$chamadoForDB['Chamadomsg'][0]['user_id'] = $this-> logged-> User-> id;
 			$chamadoForDB['Chamadomsg'][0]['msg'] = $this-> request-> data['Chamadomsg'][0]['msg'];
-			//pr($chamadoForDB);
-			//exit;
 			$salvou = $this-> Chamado-> saveAll($chamadoForDB);
 			$this-> log('salvou a marcação do email no banco: ');
 			$this-> log($salvou);
-			//exit;
 			$this-> Session-> setFlashSucesso( "Você postou uma resposta no chamado");
 			$this-> redirect(array("plugin"=> "administracao", 'controller' => 'chamados', 'action' => 'ver', $this-> Chamado-> id));
 		}
 		$this-> jsExtra[] = array('file'=> $this-> request-> base . '/administracao/ckeditor/ckeditor.js', 'comment'=> "CKEditor para edição de texto rico", 'shortPath' => false);
-		//$this-> jsExtra[] = array('file'=> $this-> request-> base . '/administracao/js/functionsArtigo.js', 'comment'=> "Por enquanto chama o CKEditor e o contacaracters", 'shortPath' => false);
 		$this-> set('jsExtra', $this-> jsExtra);
 		$this-> set('title_for_layout', "Responder: " . $chamado['Chamado']['titulo']);
 		$this-> request-> data = $this-> Chamado-> data;
