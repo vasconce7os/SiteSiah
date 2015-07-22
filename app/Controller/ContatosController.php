5697bd870c72686139a9fd9d62ae40b2d90b4048
Cerebro Vasconcelos <cerebro.vasconcelos@zipmail.com.br>
2015-07-22 12:43:59 -0400

    Adicione a permição para user não identificado
    E também troque a classe de Camado para Contato

--- app/Controller/ContatosController.php
+++ app/Controller/ContatosController.php
@@ -5,7 +5,7 @@
 App::uses('AppController', 'Controller');
 
 
-class ChamadosController extends AppController 
+class ContatosController extends AppController 
 {
 
 	//public $uses = array();
@@ -13,7 +13,7 @@ class ChamadosController extends AppController
 
 	public function index() 
 	{
-		$this-> set('title_for_layout', "Chamado com a SIAH");
+		$this-> set('title_for_layout', "Contato com a SIAH");
 		/*
 		já não precisa mais, estão no layout default
 		$this-> cssExtra[] = array('file'=> 'css/bootstrap.min', 'comment'=> "Css 2 col", 'shortPath'=> true, 'media'=> "all");
@@ -29,14 +29,14 @@ class ChamadosController extends AppController
 			//$this-> log("no enviar: é post ");
 			//$this-> log("no enviar: post: " . print_r($this-> data, 1));
 			$contatoForDB = $this-> data;
-			$contatoForDB['Chamado']['cliente_id'] = 1; // cliente não identificado
+			$contatoForDB['Contato']['cliente_id'] = 1; // cliente não identificado
 			
-			$enviou = $this-> Chamado-> save($contatoForDB);
+			$enviou = $this-> Contato-> save($contatoForDB);
 			//pr($contatoForDB);
 			//exit;
 
 			$data = $this-> request-> data;
-			//$enviou = $this-> Chamado -> save($data);
+			//$enviou = $this-> Contato -> save($data);
 			//var_dump($enviou);
 
 			if($enviou)
@@ -45,7 +45,7 @@ class ChamadosController extends AppController
 				//exit;
 				$view = new View($this, false);
 				$view->set('contatoInDB', $data);
-				$view->viewPath = 'Chamados';
+				$view->viewPath = 'Contatos';
 				//$this-> layout = 'ajax';
 				$conteudo = $view->render('enviar', 'ajax');
 				//echo"antes ";
@@ -53,13 +53,13 @@ class ChamadosController extends AppController
 				//echo" depois ";
 				$this->log('Antes da parada do email');
 				//exit;
-				$retEmail =        true; //$this-> enviaEmail("site@oseuestoque.com", 'Chamado via site de: '.@$data['Chamado']['nome'], $conteudo, 'O seu estoque');
-				$retEmailCliente = $this-> enviaEmail($data['Chamado']['email'], 'Recebemos seu contato '.@$data['Chamado']['nome'], $conteudo, $this-> sisCliente['nome']);
+				$retEmail =        true; //$this-> enviaEmail("site@oseuestoque.com", 'Contato via site de: '.@$data['Contato']['nome'], $conteudo, 'O seu estoque');
+				$retEmailCliente = $this-> enviaEmail($data['Contato']['email'], 'Recebemos seu contato '.@$data['Contato']['nome'], $conteudo, $this-> sisCliente['nome']);
 				/*
 				if($retEmail == true)
 				{
 					$this->log('Enviou para o Adm');
-					$this-> Session->setFlash('Obrigado por nos contactar '.@$data['Chamado']['nome']
+					$this-> Session->setFlash('Obrigado por nos contactar '.@$data['Contato']['nome']
 							, 'default'
 							, array('class' => 'noticeSuccess')); //noticeSuccess, notice, message
 					$this-> redirect(array("controller"=> "contatos", "action"=> "index"));
@@ -74,7 +74,7 @@ class ChamadosController extends AppController
 				if($retEmail == true)
 				{
 					$this->log('Enviou para o contatante');
-					$this-> Session->setFlash('Obrigado por nos contactar '.@$data['Chamado']['nome']
+					$this-> Session->setFlash('Obrigado por nos contactar '.@$data['Contato']['nome']
 							, 'default'
 							, array('class' => $this-> successMsgClass)); 
 					$this-> redirect(array("controller"=> "pages", "action"=> "inicio"));
@@ -98,5 +98,11 @@ class ChamadosController extends AppController
 		{
 			$this-> log("no enviar: NÃO é post ");
 		}
+	}
+
+	public function beforeFilter()
+	{
+		parent::beforeFilter();
+		$this-> Auth-> allow();
 	}
 }
