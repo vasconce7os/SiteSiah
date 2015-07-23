819886c88d06aa192702380f5e9749ea81d1425f
Cerebro Vasconcelos <cerebro.vasconcelos@zipmail.com.br>
2015-07-23 12:08:43 -0400

    Adicione validação no model Cliente

--- app/Plugin/administracao/Model/Cliente.php
+++ app/Plugin/administracao/Model/Cliente.php
@@ -2,4 +2,20 @@
 class Cliente extends AdministracaoAppModel
 {
 	var $hasOne = array('User');
+		public $validate = array(
+		    'email' => array
+			(
+		        'notEmpty' => array
+				(
+		            'rule' => 'notEmpty',
+					'required' => true,
+					'allowEmpty' => false,
+		            'message' => 'Não ode ser vazio'
+		        ),
+		        'uniqueRule' => array(
+		            'rule' => 'isUnique',
+		            'message' => 'E-mail já cadastrado!'
+		        )
+	    	)
+	);
 }
